f6a894b2da67c5907dc316809fda32b17147e6cb
raffipython <raffi@jubrael.com>
2025-12-13 09:20:18 -0500

    updates

--- 2025/5/solve1.py
+++ 2025/5/solve1.py
@@ -15,24 +15,30 @@ ranges = []
 for r in rangesTemp:
     ranges.append([int(r.split("-")[0]),int(r.split("-")[1])])
 
-items = lines[empty+1:]
+#items = lines[empty+1:]
+ranges = sorted(ranges)
+total = []
 
-def is_in_range(x):
-    for y in ranges:
-        if x >= int(y[0]) and x <= int(y[1]):
-            return True
-    return False
 
-for r in ranges:
+########################
+i = 0
+for r in ranges[:-1]:
+    print("=============")
+    print(r)
     start = r[0]
     end =   r[1]
-    size = len(str(start))
+    #size = len(str(start))
     diff = end - start
+    #print(start)
+    #print(end)
+    #print(diff)
+    #[total.append(x) for x in range(start, end+1)]
+    print(f"end of this:    {end}")
+    print(f"start of next:  {ranges[i+1][0]}")
+    print(ranges[i+1][0] - end)
+    i += 1
 
-total = 0
-for x in items:
-    size = len(x)
-    is_in = is_in_range(int(x))
-    if is_in:
-        total += 1
-print(total)
+
+#print(set(total))
+print("\n\nFINAL+++++++++")
+print(len(set(total)))
