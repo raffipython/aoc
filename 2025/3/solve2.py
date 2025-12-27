bb87d42f72d0c765f951b8b9eed4935ce5ec6f4b
raffipython <raffi@jubrael.com>
2025-12-27 17:16:12 -0500

    testing recursive for 2025 day 3

--- 2025/3/solve2.py
+++ 2025/3/solve2.py
@@ -6,60 +6,42 @@ i = 'input2'
 with open(i, 'r') as fd:
     f = fd.read()
     lines = f.split('\n')
-#print(lines)
-totalfinal = 0
+
+def next(x):
+    max = 0
+    for i in x:
+        if int(i) > max:
+            max = int(i)
+    return max        
 
 #####################
 for line in lines[:]:
-    print("-----------------")
-    l = 0
-    print(line)
-    print(line[:-11])
-    line = line[:-11]
-    for i in line:
-        if int(i) > l:
-            l = int(i)
-    max = l # max digit in line
+    answer = []
+    pointer = -12
+    #print("+++++++++++")
+    temp = line[:pointer]
+    max = next(temp)
+    #print(max)
+    answer.append(str(max))
+    #print("-----------")
+    for r in range(1,13):
+        #print(f"=>{r}")
+        temp = line[pointer:]
+        #print(line)
+        print(temp)
+        max = next(temp)
+        #print(max)
+        answer.append(str(max))
+        pointer += 1
+    #print(f"+++>>>> {answer}")
+    print("".join(answer))
+    
 
-    print(line.index(str(max)))
 
 
-    cmax = line.count(str(l)) # count of that max
-    second = None
-    if cmax == 1:
-        if int(line[-1]) == l:
-            x = l # second digit
-            found = False
-            c = 1 # local counter
-            while not found:
-                temp = line.count(str(l-c))
-                if temp > 0:
-                    found = True
-                else:
-                    c += 1
-            y = l-c
-            z = f"{y}{x}"
-            print(z)
-            totalfinal += int(z)
-        else:
-            x = l # first digit
-            found = False
-            c = 1 # local counter
-            templine = line[line.index(str(l))+1:]
-            #print(templine)
-            templ = 0
-            for i in templine:
-                if int(i) > templ:
-                    templ = int(i)
-            tempmax = templ # max digit in line    
-            z = f"{x}{tempmax}"
-            print(z)
-            totalfinal += int(z)
-    else:
-        z = int(f"{l}{l}")
-        print(z)
-        totalfinal += z
+
+
 
 #####################
 print("----")
-print(totalfinal)
+print()
